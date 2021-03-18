d5f958d7e2012915f318421c5eccb850e2722421
Mansi <mansi.budhiraja@gmail.com>
2021-03-17 21:18:12 -0700

    added two new functions

--- backend/calculator.py
+++ backend/calculator.py
@@ -5,15 +5,26 @@ def sum(operand1, operand2):
 def subtract(operand1, operand2):
     return operand1 - operand2
 
+def multiplication(operator1, operator2):
+    return operator1 * operator2
+
+def division(operator1, operator2):
+    if operator2 == 0:
+        raise Exception("division by zero not allowed")
+    else:
+        return operator1 /operator2
 
 operator_to_func_map = {
     '+': sum,
-    '-': subtract
+    '-': subtract,
+    '*': multiplication,
+    '/': division
 }
 
 
 def is_operator_supported(operator):
-    return operator in ['+', '-']
+    return operator in ['+', '-', '*', '/']
+
 
 
 def perform_operation(oper1, oper2, operator):
--- backend/tests/test_calculator.py
+++ backend/tests/test_calculator.py
@@ -20,5 +20,31 @@ class TestCalculator(unittest.TestCase):
         actual_difference = subtract(operand1, operand2)
         self.assertEqual(expected_difference, actual_difference, "Expected and actual difference differ")
 
+    def test_multiplication(self):
+        operand1 = 5
+        operand2 = -3
+
+        expected_multiplier = -15
+        actual_multiplier = multiplication(operand1, operand2)
+        self.assertEqual(expected_multiplier, actual_multiplier, "Expected and actual multiplier differ")
+
+    def test_division_zero(self):
+        operand1 = 3
+        operand2 = 0
+
+        expected_expection = Exception("division by zero not allowed")
+        try:
+            division(operand1, operand2)
+        except Exception as actual_exception:
+            self.assertEqual(str(expected_expection), str(actual_exception), "Division by zero not valid")
+
+    def test_division(self):
+        operand1 = 10
+        operand2 = 2
+
+        expected_dividend = 5.0
+        actual_dividend = division(operand1, operand2)
+        self.assertEqual(expected_dividend, actual_dividend, "Expected and actual dividend differ")
+
 if __name__ == '__main__':
     unittest.main()
